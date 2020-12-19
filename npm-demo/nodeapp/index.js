14208cb029c8a30d1af1c57e52ebb96e5da353f3
sameerasad <ssameerkhan525@gmail.com>
2020-12-19 17:16:25 +0500

    publish custom module and use it module name=cheeta-lib-11112

---
+++ npm-demo/nodeapp/index.js
@@ -0,0 +1,7 @@
+const addition = require("../cheeta-lib/index");
+const add = require("cheeta-lib-11112");
+console.log(add);
+
+const result = add.sum(0, 5);
+console.log(result);
+console.log(add.sum(4.5, 6));
